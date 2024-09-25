c048bce15fb08cc983cef89ae28e6604c4f084c9
ashutoshphegade <phegade.ashutosh@gmail.com>
2024-09-25 21:41:28 +0530

    assignment 5 complete

---
+++ As3_extintrupt_v2/Inc/switch_intr.h
@@ -0,0 +1,25 @@
+/*
+Author: Nilesh Ghule <nilesh@sunbeaminfo.com>
+Course: PG-DESD @ Sunbeam Infotech
+Date: Sep 21, 2024
+*/
+
+#ifndef SWITCH_INTR_H_
+#define SWITCH_INTR_H_
+
+#include "stm32f4xx.h"
+
+#define GPIO_SWITCH				GPIOA
+#define SWITCH_PIN				0
+#define SWITCH_GPIO_CLKEN		0
+
+#define SWITCH_EXTI				0
+
+void SwitchInit(void);
+void EXTI0_IRQHandler(void);
+
+extern uint32_t SwitchExtiFlag;
+
+extern uint32_t i;
+
+#endif /* SWITCH_INTR_H_ */
